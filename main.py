1d9c67441bcb30566092a73d20be62901ce659fc
Aditya Datta <ADITYADATTAOFFICIAL@GMAIL.COM>
2023-09-19 12:10:17 +0530

    Deploy FastAPI to Vercel

--- main.py
+++ main.py
@@ -90,5 +90,5 @@ def predict(link: str):
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
