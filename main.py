739a71a6d7a74127cf7660f932a20fbe046cf182
Aditya Datta <ADITYADATTAOFFICIAL@GMAIL.COM>
2023-09-20 19:44:56 +0530

    'upload-model'

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+
 import pickle
 import uvicorn
 from pydantic import BaseModel
+
+from tensorflow import keras
 import numpy as np
+from fastapi import FastAPI, HTTPException
+
+# Import the necessary functions and classes from your model code
+from tensorflow.keras.preprocessing.text import one_hot
+
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -13,35 +22,11 @@ with open("tokenizer.pkl", "rb") as file:
     tokenizer = pickle.load(file)
 
 # Load your LSTM model (assuming it's defined elsewhere)
-lstm = keras.models.load_model("sentiment_model.h5")
-def pad_seq(sequences, maxlen, padding_value=0):
-    """
-    Pad a list of sequences to a specified maximum length.
+lstm = load_model("sentiment_model.h5")
+model = load_model('emix.model.h5')  # Replace with the path to your saved model
 
-    Args:
-    sequences (list of lists): The input sequences, where each sequence is a list of integers.
-    maxlen (int): The desired maximum sequence length.
-    padding_value (int, optional): The value used for padding. Default is 0.
-
-    Returns:
-    numpy.ndarray: An array containing the padded sequences.
-    """
-    num_sequences = len(sequences)
-    padded_sequences = np.zeros((num_sequences, maxlen), dtype=int)
-
-    for i, seq in enumerate(sequences):
-        if len(seq) <= maxlen:
-            padded_sequences[i, :len(seq)] = seq
-        else:
-            padded_sequences[i, :] = seq[:maxlen]
-
-    return padded_sequences
-
-
-
-
-
-
+# Define the label mapping for emotions
+label_mapping = {0: 'anger', 1: 'fear', 2: 'joy', 3: 'sadness', 4: 'neutral', 5: 'surprise', 6: 'shame', 7: 'disgust'}
 
 def clean_tweet(tweet):
     # Remove URLs
@@ -72,13 +57,17 @@ def scrape_text_from_link(link):
     except Exception as e:
         return str(e)
 
-
+def preprocess_data(data):
+    vocab_size = 5000  # Should match the vocab size used during model training
+    onehot_repr = [one_hot(words, vocab_size) for words in [data]]
+    sent_length = 50  # Should match the sequence length used during model training
+    docs = pad_sequences(onehot_repr, padding='pre', maxlen=sent_length)
+    return docs
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 @app.get("/predict")
 def predict(link: str):
     scraped_text = scrape_text_from_link(link)
@@ -87,12 +76,18 @@ def predict(link: str):
         return {"error": "Unable to scrape text from the provided link"}
 
     X = clean_tweet(scraped_text)
-    
+    preprocessed_text = preprocess_data(scraped_text)
+
+        # Make a prediction using the loaded model
+    e_prediction = model.predict(preprocessed_text)
+
+        # Get the predicted emotion label
+    predicted_label = label_mapping[np.argmax(e_prediction)]
     # Tokenize the text and convert to sequence of token IDs
     X_seq = tokenizer.texts_to_sequences([X])
     
     # Pad the sequence of token IDs
-    X_seq = pad_seq(X_seq, maxlen=100)  # Use X_seq instead of X
+    X_seq = pad_sequences(X_seq, maxlen=100)  # Use X_seq instead of X
     
     # Assuming you have loaded the lstm model earlier
     custom_sentiment_prob = lstm.predict(X_seq)[0][0]
@@ -112,9 +107,9 @@ def predict(link: str):
     # Print the sentiment and probability score
     print(f"Sentiment: {custom_sentiment}")
     print(custom_sentiment_prob)
-    return {"text": scraped_text, "prediction": float(custom_sentiment_prob), "sentiment": custom_sentiment}
+    return {"text": scraped_text, "prediction": float(custom_sentiment_prob), "sentiment": custom_sentiment,"emotion": predicted_label}
 
 # 5. Run the API with uvicorn
 #    Will run on http://127.0.0.1:8000
-# if __name__ == "__main__":
-    # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
